c350ef3ec8ebc7ef107cc022738eacb106e19e6c
newdeveloper1010 <139791366+newdeveloper1010@users.noreply.github.com>
2023-08-30 07:21:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,12 @@
-const form = document.getElementById('form-confirm');
 
-let formValid = false;
+	function adicionarLinha() {
+	  var nome = document.getElementById("nome").value;
+	  var telefone = document.getElementById("telefone").value;
 
-function numbervalid (ValA, ValB){
-    return ValB > ValA;
-}
-
-form.addEventListener('submit', function(e){
-    e.preventDefault(); //previne carregamento da página após "submit"
-    let ValA = parseFloat(document.getElementById('valor-a').value);
-    let ValB = parseFloat(document.getElementById('valor-b').value);
-
-    const messagesucess = 'formulario enviado com sucesso! Verificando: <b> ValorA menor que ValorB!</B>';
-    const containermessagesucess = document.querySelector('.success-message');
-    
-    formValid = numbervalid(ValA, ValB);
-    
-    if (formValid){
-        document.querySelector('.error-message').style.display = 'none';
-        containermessagesucess.innerHTML = messagesucess;
-        containermessagesucess.style.display = 'block';
-        document.querySelector('.error-message').style.display = 'none';
-
-        ValA.value = '';
-        ValB.value = '';
-    } else {
-        document.querySelector('.error-message').style.display = 'block';
-        containermessagesucess.style.display = 'none';
-    }    
-})
+      var tabela = document.getElementById("tabela");
+	  var linha = tabela.insertRow(-1);
+	  var coluna1 = linha.insertCell(0);
+	  var coluna2 = linha.insertCell(1);
+	  coluna1.innerHTML = nome;
+	  coluna2.innerHTML = telefone;
+	}
